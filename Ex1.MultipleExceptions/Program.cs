d61e5c0bf83933e40cccbab437571016b2ade1d0
Flavio <flaviocampelo@hotmail.com>
2022-09-13 21:56:26 -0400

    Initial commit

---
+++ Ex1.MultipleExceptions/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Task firstTask = DoSomethingAsync("1st", 5, 3);
+Task secondTask = DoSomethingAsync("2nd", 5, 4, true);
+Task thirdTask = DoSomethingAsync("3rd", 5, 3, true);
+
+
+async Task DoSomethingAsync(string taskName, int loops, int waitTimeInSeconds, bool throwException = false)
+{
+    for (int i = 0; i < loops; i++)
+    {
+        Console.WriteLine($"progress of {taskName} task");
+        await Task.Delay(TimeSpan.FromSeconds(waitTimeInSeconds));
+        if (throwException)
+            throw new ArgumentException($"Exception of {taskName} task");
+    }
+}
+
+Task.WaitAll(firstTask, secondTask, thirdTask);
+Console.WriteLine("Everything is done");
---
+++ Ex1.MultipleExceptions/README.md
@@ -0,0 +1,4 @@
+# Ex1.MultipleExceptions
+When multiple exception are thrown, an AggregateException will be catch all of them. So, you can look at inner exceptions to find further details about each exception.
+
+![Image 1](./assets/img1.png)
